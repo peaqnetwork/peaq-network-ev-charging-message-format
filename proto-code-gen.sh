4f5ec63c3f31d334aa22b361b3eea6198115cf09
jaypan <pan@advancedblockchain.com>
2022-03-23 13:30:12 +0100

    Update all languange

--- proto-code-gen.sh
+++ proto-code-gen.sh
@@ -1,9 +1,9 @@
 # # Golang code generator
-# protoc -I=./ --go_opt=paths=source_relative --go_out golang/message $(find ./ -iname "*.proto")
+protoc -I=./ --go_opt=paths=source_relative --go_out golang/message $(find ./ -iname "*.proto")
 
-# # Rust code generator
-# protoc -I=./  --rust_out rust/src $(find ./ -iname "*.proto")
-# #rename the mod.rs to lib.rs
-# mv rust/src/mod.rs rust/src/lib.rs
+# Rust code generator
+protoc -I=./  --rust_out rust/src $(find ./ -iname "*.proto")
+#rename the mod.rs to lib.rs
+mv rust/src/mod.rs rust/src/lib.rs
 
 protoc -I=./ --python_out python $(find ./ -iname "*.proto")
