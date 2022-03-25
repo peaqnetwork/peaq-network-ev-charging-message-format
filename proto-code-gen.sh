82829f54886e560cbc6686c4c3c1f063efcd72ac
Iredia Ebikade <40021650+irediaes@users.noreply.github.com>
2022-03-25 11:03:52 +0100

    feat: added did document proto definitions and protobuf generator

--- proto-code-gen.sh
+++ proto-code-gen.sh
@@ -1,7 +1,9 @@
 # Golang code generator
-protoc -I=./ --go_opt=paths=source_relative --go_out golang/message $(find ./ -iname "*.proto")
+protoc -I=./ --go_opt=paths=source_relative --go_out golang/message $(find ./ -iname "p2p_message_format.proto")
+protoc -I=./ --go_opt=paths=source_relative --go_out golang/document $(find ./ -iname "did_document_format.proto")
 
 # Rust code generator
 protoc -I=./  --rust_out rust/src $(find ./ -iname "*.proto")
+# protoc -I=./  --rust_out rust/src $(find ./ -iname "did_document_format.proto")
 #rename the mod.rs to lib.rs
 mv rust/src/mod.rs rust/src/lib.rs
--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,4 @@
 // @generated
 
+pub mod did_document_format;
 pub mod p2p_message_format;
